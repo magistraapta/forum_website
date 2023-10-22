c6518f4256b539f8d51332baccd708e6b42788ff
Magistra <magistraagis@gmail.com>
2023-10-22 15:11:55 +0800

    create endpoints

--- index.js
+++ index.js
@@ -1,36 +1,81 @@
-const { PrismaClient } = require('@prisma/client');
+const { Prisma, PrismaClient } = require('@prisma/client');
+const express = require('express');
+const { connect } = require('http2');
 
+const app = express();
 const prisma = new PrismaClient();
+const port = 3000;
+app.use(express.json());
 
-async function main() {
-  const newUser = await prisma.user.create({
-    data: {
-      name: 'testing',
-      email: 'testing@gmail.com',
-      Post: {
-        create: {
-          title: 'hello',
-        },
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`);
+});
+
+app.get('/users', async (req, res) => {
+  const findUser = await prisma.user.findMany();
+  res.json(findUser);
+});
+
+app.post('/sign-up', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const createUser = await prisma.user.create({
+      data: {
+        name,
+        password,
+        email,
       },
-      Profile: {
-        create: {
-          bio: 'i like coding',
-        },
+    });
+
+    res.status(200).json({ message: 'user created successfully', user: createUser });
+  } catch (error) {
+    if ((error.code = 'P2002' && error.meta.target.includes('email'))) {
+      res.status(400).json('email already exist');
+    } else {
+      res.status(500).json('create user failed');
+    }
+    console.log(error);
+  }
+});
+
+app.post('/post', async (req, res) => {
+  const { title, content, userEmail } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: userEmail
+        }
+    })
+    const result = await prisma.post.create({
+      data: {
+        title,
+        content,
+        author: {connect: {email: user.email}} ,
       },
-    },
-  });
+    });
+    res.status(200).json({message: 'success create post', result});
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-//   const findUser = await
+app.post('/login', async (req, res) => {
+  const { name, password } = req.body;
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        name: name,
+        password: password,
+      },
+    });
 
-  console.log(newUser)
-}
+    if (!user) {
+      return res.status(401).json({ error: 'invalid name or password!' });
+    }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    res.json('success login');
+  } catch (error) {
+    res.json(error);
+  }
+});
