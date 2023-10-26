c5048ac734d7c8eaba7036c2f04c370aaf2f0614
Magistra <magistraagis@gmail.com>
2023-10-26 14:50:27 +0800

    add comment endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 const prisma = new PrismaClient();
-const port = 3000;
+const port = 8888;
 app.use(express.json());
 
 app.listen(port, () => {
@@ -27,13 +27,34 @@ app.post('/sign-up', async (req, res) => {
       },
     });
 
-    res.status(200).json({ message: 'user created successfully', user: createUser });
+    res.status(200).json({ message: 'success', user: createUser });
   } catch (error) {
     if ((error.code = 'P2002' && error.meta.target.includes('email'))) {
       res.status(400).json('email already exist');
     } else {
       res.status(500).json('create user failed');
     }
+    console.log(error);
+  }
+});
+
+app.get('/post/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await prisma.post.findUnique({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        comments: {orderBy:{id: 'asc'}}
+      },
+    });
+    res.status(200).json({
+      message: 'success',
+      post: result,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
     console.log(error);
   }
 });
@@ -88,3 +109,33 @@ app.get('/feed', async (req, res) => {
     res.status(500).json({ error: 'error getting feed' });
   }
 });
+
+app.post('/post/:id/comment', async (req, res) => {
+  const { id } = req.params;
+  const { comment, username } = req.body;
+  try {
+    const findPost = await prisma.post.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!findPost) {
+      res.status(404).json({ message: 'post no found' });
+    }
+
+    const result = await prisma.comment.create({
+      data: {
+        comment: comment,
+        post: { connect: { id: findPost.id } },
+        user: username,
+      },
+    });
+    res.status(200).json({ message: 'success', response: result });
+  } catch (error) {
+    res.status(500).json({ message: error });
+    console.log(error);
+  }
+});
+
+
