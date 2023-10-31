2d79321276d2b997552f66726a2ee9ba8a455652
Magistra <magistraagis@gmail.com>
2023-10-31 14:21:08 +0800

    add comment controller

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,4 +1,4 @@
-const {prisma} = require('../utils/db');
+const { prisma } = require('../utils/db');
 
 async function createPost(req, res) {
   const { title, content, userEmail } = req.body;
@@ -75,4 +75,50 @@ async function showAllPosts(req, res) {
   }
 }
 
-module.exports = {getPostById,createPost, showAllPosts }
+async function createComment(req, res) {
+  const { id } = req.params;
+  const { comment, userEmail } = req.body;
+  try {
+    const findPost = await prisma.post.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!findPost) {
+      res.status(404).json({ message: 'post not found' });
+    }
+
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+
+    if (!findUser) {
+      res.status(404).json({ message: 'post not found' });
+    }
+
+    const result = await prisma.comment.create({
+      data: {
+        comment: comment,
+        post: { connect: { id: findPost.id } },
+        author: { connect: { email: findUser.email } },
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'internal server error' });
+  }
+}
+
+module.exports = { getPostById, createPost, showAllPosts, createComment };
