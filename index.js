8f23e493ba5e828c7cae7c59bdee726cd39a14a2
Magistra <magistraagis@gmail.com>
2023-11-01 13:22:37 +0800

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const userRouter = require('./router/user.router.js')
 const authRouter = require('./router/auth.router.js')
 const postRouter = require('./router/post.router.js')
+const cors = require('cors')
 
 const app = express();
 const port = 8888;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/user', userRouter)
