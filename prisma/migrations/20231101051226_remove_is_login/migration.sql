c4dcade49d669f384654058c72c0fb24b369657c
Magistra <magistraagis@gmail.com>
2023-11-01 13:23:55 +0800

    add cors

---
+++ prisma/migrations/20231101051226_remove_is_login/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isLogin` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "isLogin";
---
+++ prisma/migrations/20231101051644_add_is_login/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isLogin" BOOLEAN NOT NULL DEFAULT false;
