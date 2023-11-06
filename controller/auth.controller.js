d47d9b6c3fc47e557b5f6922e506895651d70081
Magistra <magistraagis@gmail.com>
2023-11-06 13:42:35 +0800

    create token function

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,74 @@
-const {prisma} = require('../utils/db');
+const { prisma } = require('../utils/db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
+
+const { hashToken } = require('../utils/hashToken');
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '3m' });
+}
+
+function generateRefreshToken(user, jti) {
+  return jwt.sign(
+    {
+      userId: user.id,
+      jti,
+    },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: '8h' }
+  );
+}
+
+function generateTokens(user, jti) {
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user, jti);
+
+  return {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  };
+}
+
+function addRefreshTokenToWhitelist({ jti, refreshToken, userId }) {
+  return prisma.token.create({
+    data: {
+      id: jti,
+      token: hashToken(refreshToken),
+      userId,
+    },
+  });
+}
+
+function findRefreshTokenById(id) {
+  return prisma.refreshToken.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+function deleteRefreshToken(id) {
+  return prisma.refreshToken.update({
+    where: {
+      id,
+    },
+    data: {
+      revoked: true,
+    },
+  });
+}
+
+function revokeTokens(userId) {
+  return prisma.refreshToken.updateMany({
+    where: {
+      userId,
+    },
+    data: {
+      revoked: true,
+    },
+  });
+}
+
 async function signup(req, res) {
   const { name, email, password } = req.body;
 
@@ -15,7 +83,7 @@ async function signup(req, res) {
       res.status(403).json({ message: 'email already been use' });
     }
 
-    const hashPassword = await bcrypt.hash(password, 10)
+    const hashPassword = await bcrypt.hash(password, 10);
     const createUser = await prisma.user.create({
       data: {
         name,
@@ -24,13 +92,17 @@ async function signup(req, res) {
       },
     });
 
-    res.status(200).json({ message: 'success', user: createUser });
+    const jti = uuidv4();
+    const { accessToken, refreshToken } = generateTokens(createUser, jti);
+    await addRefreshTokenToWhitelist({ jti, refreshToken, userId: createUser.id });
+
+    res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken });
   } catch (error) {
     console.log(error);
   }
 }
 
-async function login(req,res){
+async function login(req, res) {
   const { email, password } = req.body;
   try {
     const user = await prisma.user.findUnique({
@@ -44,32 +116,20 @@ async function login(req,res){
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
+    const accessToken = generateAccessToken(user);
+    const refreshToken = jwt.sign(user, process.env.JWT_REFRESH_SECRET);
 
     if (isPasswordValid) {
-      const payload = {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-      } 
-
-      const secret = process.env.JWT_SECRET
-      const expireIn = 60 *60 *1
-      const token = jwt.sign(payload, secret, {expiresIn: expireIn} )
-      return res.json({
-        data: {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          token: token
-        },
+      res.json({
+        accessToken: accessToken,
+        refreshToken: refreshToken,
       });
     } else {
-      res.status(403).json({ message: 'wrong password' });
+      res.status(403).json('wrong password');
     }
-
   } catch (error) {
     res.json(error);
   }
 }
 
-module.exports = {login, signup}
+module.exports = { login, signup };
